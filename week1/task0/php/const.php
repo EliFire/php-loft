fbfba216160cbce7617555a0328ae85ba7dda668
EliSha <elinshark@gmail.com>
2021-01-16 16:08:43 +0300

    Задание 0 от первой недели 16.01.2020

---
+++ week1/task0/php/const.php
@@ -0,0 +1,3 @@
+<?php
+
+const TEST_CINSTANTA = 'asd';
---
+++ week1/task0/task1.0.php
@@ -0,0 +1,19 @@
+<?php
+
+/*Приведите этот код к соответствию стандарту PSR-2
+<?php
+CONST testConstanta = 'asd';
+$user_name = "Igor";
+
+function show_something(){ RETURN 'SOMETHING';}
+
+if (1==1) echo 'hi';*/
+
+include 'php/const.php';
+include 'php/func.php';
+
+$user_name = "Igor";
+
+if (1 == 1) {
+    echo 'hi';
+}
